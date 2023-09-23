b81bf8e59f60ce42791f47b58290430cb76ba83c
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-09-23 12:30:14 +0400

    Initial Commit

---
+++ TodoApp1/TodoApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp1App.swift
+//  TodoApp1
+//
+//  Created by Abdulla Saeed Alblooshi on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
