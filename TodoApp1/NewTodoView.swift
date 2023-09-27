ff2be35f3f23a53666e626bdb4f5f77d988ce376
Abdulla-Saeed <101017918+Abdulla-Saeed@users.noreply.github.com>
2023-09-27 22:55:44 +0400

    Added an edit button which enables the user to delete and reorder todos.

---
+++ TodoApp1/NewTodoView.swift
@@ -0,0 +1,20 @@
+//
+//  NewTodoView.swift
+//  TodoApp1
+//
+//  Created by Abdulla Saeed Alblooshi on 01/10/2023.
+//
+
+import SwiftUI
+
+struct NewTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTodoView()
+    }
+}
